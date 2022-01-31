ddd5ce9b70c166ee92a3b143d132c1133e11f36f
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-01-31 15:48:16 -0300

    Full App Ready to deploy
    Count down feature

---
+++ client-comming-soon/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import image1 from "./img/Create-bro.png";
+import CounterContainer from "./components/CounterContainer/CounterContainer";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h3 className="title">we are comming soon</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
+          possimus sequi ab libero consectetur dicta ipsam placeat? Deleniti,
+          pariatur magnam, impedit repudiandae maiores rerum dolores ad natus
+          aliquam, tempore quo!
+        </p>
+        <CounterContainer />
+        <a href="#" className="btn">
+          notify me
+        </a>
+      </div>
+      <div className="image">
+        <img src={image1} alt="Imagen1" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client-comming-soon/src/components/CounterContainer/CounterContainer.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+
+const CounterContainer = () => {
+  const [countDate, setCountDate] = useState(
+    new Date("feb 5, 2022 12:00:00").getTime()
+  );
+
+  const [horaRestante, setHoraRestante] = useState({
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+  });
+
+  function countDown() {
+    let now = new Date().getTime();
+    let gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    setHoraRestante({
+      day: Math.floor(gap / day),
+      hour: Math.floor((gap % day) / hour),
+      minute: Math.floor((gap % hour) / minute),
+      second: Math.floor((gap % minute) / second),
+    });
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      countDown();
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="count-down">
+      <div className="box">
+        <h3 id="day">{horaRestante.day === 0 ? "00" : horaRestante.day}</h3>
+        <span>day</span>
+      </div>
+      <div className="box">
+        <h3 id="hour">{horaRestante.hour === 0 ? "00" : horaRestante.hour}</h3>
+        <span>hour</span>
+      </div>
+      <div className="box">
+        <h3 id="minute">
+          {horaRestante.minute === 0 ? "00" : horaRestante.minute}
+        </h3>
+        <span>minute</span>
+      </div>
+      <div className="box">
+        <h3 id="second">
+          {horaRestante.second === 0 ? "00" : horaRestante.second}
+        </h3>
+        <span>second</span>
+      </div>
+    </div>
+  );
+};
+
+export default CounterContainer;
