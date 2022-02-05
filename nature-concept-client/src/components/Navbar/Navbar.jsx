837342ac9e19fe63a737e4b5199fc87ce4623c54
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-05 15:29:29 -0300

    Navbar component

---
+++ nature-concept-client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <a href="#" className="logo">
+        logo <i className="fas fa-leaf"></i>
+      </a>
+
+      <nav>
+        <ul>
+          <li style={{ i: 1 }}>
+            <a href="#">home</a>
+          </li>
+          <li style={{ i: 2 }}>
+            <a href="#">about</a>
+          </li>
+          <li style={{ i: 3 }}>
+            <a href="#">contact</a>
+          </li>
+          <li style={{ i: 4 }}>
+            <a href="#">login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
